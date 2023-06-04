bdea49820b72f4201208803cbf7ed5cb1e7538a8
Ines Haouala <ines.ha13@hotmail.fr>
2023-06-04 18:16:34 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,10 +1,13 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('.'))
+
 
 project = 'RT2_CW2_sphinx'
 copyright = '2023, Ines Haouala'
@@ -12,7 +15,6 @@ author = 'Ines Haouala'
 release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [ 'sphinx.ext.autodoc',
 'myst_parser',]
@@ -27,10 +29,22 @@ source_suffix = {
 
 
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+
+# -- Options for Autodoc extension -------------------------------------------
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+
+# Add the path to my Python scripts directory
+import os
+import sys
+sys.path.insert(0, os.path.abspath('./scripts'))
